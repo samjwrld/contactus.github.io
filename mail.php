84cd6763cf002ef14225364ef6b96de641566af0
sam <34920664+samjwrld@users.noreply.github.com>
2023-04-24 10:54:41 +0530

    1001

--- mail.php
+++ mail.php
@@ -2,14 +2,14 @@
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-$phone = $_POST['Phone'];
+$subject = $_POST['subject'];
 $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "thekoncepthouse.sam@gmail.com";
+$recipient = "example@gmail.com";
 
-mail($recipient, $phone, $message, $mailheader) or die("Error!");
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
 echo'
 
@@ -25,7 +25,7 @@ echo'
 </head>
 <body>
     <div class="container">
-        <h1>Thank you for contacting us. Will get back to you as soon as possible!</h1>
+        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
         <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
         
     </div>
